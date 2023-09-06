557fdee9fa5b9800c70879b770d9fa61338a76a0
yhakkk <yhakblanco@gmail.com>
2023-09-06 13:14:27 -0300

    Commit

--- config.js
+++ config.js
@@ -7,13 +7,29 @@ function AgregarTexto() {
       let agregar = document.createElement("div");
       agregar.id = "div-" + contador;
       contador += 1;
-      agregar.innerHTML = texto + "<button onclick='BorrarDiv(\"" + agregar.id + "\")'>Realizado</button>";
+      agregar.innerHTML = texto + "<button style='margin:0 ;' onclick='BorrarDiv(\"" + agregar.id + "\")'>Realizado</button>";
       lista.appendChild(agregar);
     }
-    
+    GuardarListaEnLocalStorage();
+
   }
 
 function BorrarDiv(idDiv) {
   let Divcreado = document.getElementById(idDiv);
   Divcreado.remove();
+  GuardarListaEnLocalStorage();
+
 }
+
+
+function GuardarListaEnLocalStorage() {
+  let lista = document.getElementById("lista");
+  localStorage.setItem("listaTareas", lista.innerHTML);
+}
+window.addEventListener("DOMContentLoaded", function() {
+  let lista = document.getElementById("lista");
+  let listaGuardada = localStorage.getItem("listaTareas");
+  if (listaGuardada) {
+    lista.innerHTML = listaGuardada;
+  }
+});
